53bd9a5c0ae31e075b0c2403661f373106c1d1a7
Frunza <frunza.andrei.daniel@gmail.com>
2020-02-24 21:59:58 +0200

    add Icon cart

---
+++ src/header/cart-elem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Icon from "../image/cart.png";
+
+const CartIcon = props => {
+  return (
+    <div className="cart-icon">
+      <img src={Icon} className="cart-icon-img" />
+      <p className="cart-number-items">{props.numberItem}</p>
+    </div>
+  );
+};
+
+export default CartIcon;
--- src/header/navbar.js
+++ src/header/navbar.js
@@ -3,14 +3,20 @@ import Logo from "./logo";
 import Menu from "./menu";
 import "./header-style.css";
 import IconHamb from "./hamburger-icon";
+import CartIcon from "./cart-elem";
 
 const Navbar = props => {
  return (
     <div className="navbar-div">
       <IconHamb displayMenu = {props.displayMenuH}/>
       <Logo lgName="MyProject" />
+      {/* just space for flex */}
+      <div></div> 
       <div></div>
+      <div></div>
+      {/* just space for flex */}
       <Menu clasaMeniu="menu-principal"/>
+      <CartIcon numberItem={0}/>
     </div>
   );
 };
